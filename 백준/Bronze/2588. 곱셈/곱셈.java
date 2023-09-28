a7ee10d1525d1e11a0cf19a99e01e2f66bb78852
Minuring <144205824+Minuring@users.noreply.github.com>
2023-09-28 14:19:58 +0900

    [Bronze III] Title: 곱셈, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        bw.write(Integer.toString(a*(b%10))+"\n");
+        bw.write(Integer.toString(a*((b%100)/10))+"\n");
+        bw.write(Integer.toString(a*(b/100))+"\n");
+        bw.write(Integer.toString(a*b)+"\n");
+        bw.close();
+    }
+}
