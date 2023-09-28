d2ffab4c0e7c1d9323d7572348ca58e4515ff21c
Minuring <144205824+Minuring@users.noreply.github.com>
2023-09-28 14:25:10 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t, a, b;
+
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << "\n";
+
+	}
+	return 0;
+
+}
