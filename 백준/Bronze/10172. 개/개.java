4b5c8da250336b19c3abdbc5868b8fca799b011b
Minuring <144205824+Minuring@users.noreply.github.com>
2023-09-28 14:22:49 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|");
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
