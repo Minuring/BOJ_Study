591bac2f2f1a70d89ee2015b4a989e748b5e0705
Minuring <144205824+Minuring@users.noreply.github.com>
2023-09-28 14:24:09 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String a = br.readLine();
+        bw.write(a+"??!");
+        bw.close();
+    }
+}
