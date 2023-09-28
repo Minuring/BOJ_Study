15b398fac932737a5cf2a47d4c111ddf9bd182ca
Minuring <144205824+Minuring@users.noreply.github.com>
2023-09-28 14:21:17 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cards[] = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        int sum=0;
+        for(int i=0; i<N-2; i++){
+            for(int j=i+1; j<N; j++){
+                for(int k=j+1; k<N; k++){
+                    int tmp=cards[i]+cards[j]+cards[k];
+                    if(tmp<=M && tmp>sum) sum=tmp;
+                }
+            }
+        }
+        bw.write(Integer.toString(sum));
+        bw.close();
+
+    }
+}
