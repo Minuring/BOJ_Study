21b43fd597ace0742a01912d438a5d24c8154368
Minuring <mw9505@gmail.com>
2023-11-01 00:13:43 +0900

    [Modify] 불필요한 변수 count 제거

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -11,17 +11,14 @@ sum = 0
 visited = [False] * (V+1)
 
 heap = [[0,1]]
-count = 0
 
 while heap :
-    if count == V : break
 
     w,u = heapq.heappop(heap)
     if visited[u] : continue
 
     visited[u] = True
     sum += w
-    count += 1
 
     # u에서 인접한 정점 탐색
     for v,wt in G[u] :
