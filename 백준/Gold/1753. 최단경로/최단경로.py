9f0514cfc035a03bcbf55275117bec6b23c87a10
Minuring <144205824+Minuring@users.noreply.github.com>
2023-11-08 01:03:08 +0900

    [Gold IV] Title: 최단경로, Time: 924 ms, Memory: 73512 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,33 @@
+import heapq,sys; input=sys.stdin.readline
+
+V, E = map(int, input().split())
+start = int(input())
+G = [[] for _ in range(V+1)]
+for i in range(E):
+    u, v, w = map(int,input().split())
+    G[u].append([v,w]) #u,v,w
+
+dist = [float('inf') for _ in range(V+1)]
+def dijkstra(s):
+    hq = []
+    heapq.heappush(hq, [0,s])
+    dist[s] = 0
+
+    while hq:
+        min_dist, min_vertex = heapq.heappop(hq)
+
+        if dist[min_vertex] < min_dist :
+            continue
+
+        for v,w in G[min_vertex]:
+            if dist[v] > min_dist+w:
+                dist[v] = min_dist+w
+                heapq.heappush(hq,[min_dist+w,v])
+
+dijkstra(start)
+for i in dist[1:]:
+    if i == float('inf') :
+        print("INF")
+    else :
+        print(i)
+
