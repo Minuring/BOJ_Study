318a33bfc97ddb5454f56d3bf705ec3356902c10
Minuring <144205824+Minuring@users.noreply.github.com>
2023-12-25 16:05:40 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 336 ms, Memory: 50332 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -0,0 +1,14 @@
+dp = [[0,0,0] for _ in range(100001)]
+dp[1] = [1,0,0]
+dp[2] = [0,1,0]
+dp[3] = [1,1,1]
+
+mod = 1000000009
+for i in range(4, 100001):
+    dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % mod
+    dp[i][1] = (dp[i - 2][0] + dp[i - 2][2]) % mod
+    dp[i][2] = (dp[i - 3][0] + dp[i - 3][1]) % mod
+
+T = int(input())
+for _ in range(T):
+    print(sum(dp[int(input())]) % mod)
