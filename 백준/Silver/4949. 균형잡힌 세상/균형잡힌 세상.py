3ec98bf8219f96102b39023f7ef3d0b476ef2144
Minuring <144205824+Minuring@users.noreply.github.com>
2024-01-07 12:57:06 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 424 ms, Memory: 34768 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,33 @@
+import re
+while True:
+    try:
+        s = input().rstrip()
+        if not s or s == '.':
+            break
+
+        l = re.findall(r'[\[\]().$]', s)
+
+        stack, result = [], False
+        for e in l:
+            if e == '.' and e == s[-1]:
+                result = False if stack else True
+                break
+
+            if e == '(' or e == '[':
+                stack.append(e)
+
+            else:
+                if not stack:
+                    break
+                if (e == ')' and stack[-1] == '(')\
+                    or (e == ']' and stack[-1] == '['):
+                    stack.pop()
+                else :
+                    break
+
+            # print(f'- {e} 처리 후 stack : {stack}')
+
+        print('yes' if result else 'no')
+
+    except EOFError:
+        break
