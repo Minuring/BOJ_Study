e0e5285597af7503c7e3696cb347d0feaea956c6
Minuring <144205824+Minuring@users.noreply.github.com>
2024-01-09 01:36:50 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,4 +1,6 @@
-import sys;input=sys.stdin.readline
+import sys
+
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
@@ -7,46 +9,27 @@ for _ in range(N):
     miro.append([int(x) for x in input().rstrip()])
 # 가로N 세로M
 
+# bfs
+from collections import deque
+def bfs():
+    deltaY, deltaX = [1, -1, 0, 0], [0, 0, -1, 1]  # 상하좌우
+    visited = [[False for _ in range(M)] for _ in range(N)]
 
-G = [[] for _ in range(N * M)]
-# 그래프로 변환
-for i in range(N):
-    for j in range(M):
-        if miro[i][j] == 0: continue
-        # G[M * i + j].append(M * i + j)
+    queue = deque()
+    queue.append((0, 0, 1))
+    while queue:
+        i, j, dist = queue.popleft()
+        if visited[i][j]:
+            continue
+        visited[i][j] = True
 
-        if i != 0 and miro[i - 1][j] == 1:
-            G[M * i + j].append(M * (i - 1) + j)
-        if i != N - 1 and miro[i + 1][j] == 1:
-            G[M * i + j].append(M * (i + 1) + j)
-        if j != 0 and miro[i][j - 1] == 1:
-            G[M * i + j].append(M * i + (j - 1))
-        if j != M - 1 and miro[i][j + 1] == 1:
-            G[M * i + j].append(M * i + (j + 1))
+        if i == N - 1 and j == M - 1:
+            return dist
 
+        for dy, dx in zip(deltaY, deltaX):
+            newI, newJ = i + dy, j + dx
+            if 0 <= newI < N and 0 <= newJ < M and \
+                miro[newI][newJ] == 1 and not visited[newI][newJ]:
+                queue.append((newI, newJ, dist + 1))
 
-dist = [float('inf') for _ in range(N * M)]
-visited = [False for _ in range(N*M)]
-def dijkstra(start):
-    global dist,visited
-    dist[start] = 1
-
-    for i in range(N*M):
-        minDist, minVertex = float('inf'), 0
-        for j in range(N*M):
-            if not visited[j] and dist[j] < minDist:
-                minVertex = j
-                minDist = dist[j]
-
-        dist[minVertex] = minDist
-        visited[minVertex] = True
-
-        if minVertex == N*M - 1:
-            break
-
-        for connected in G[minVertex]:
-            if not visited[connected] and dist[connected] > dist[minVertex] + 1:
-                dist[connected] = dist[minVertex] + 1
-
-dijkstra(0)
-print(dist[N*M-1])
+print(bfs())
