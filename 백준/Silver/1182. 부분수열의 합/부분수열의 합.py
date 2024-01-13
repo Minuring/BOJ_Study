e59768d2f33cd2e70657bb5c11b7f75cd327ada0
Minuring <144205824+Minuring@users.noreply.github.com>
2024-01-13 16:13:27 +0900

    [Silver II] Title: 부분수열의 합, Time: 248 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,15 +2,15 @@ N, S = map(int, input().split())
 nums = list(map(int, input().split()))
 
 count = 0
-def solve(i, L, curSum):
+def solve(i, curSum):
     global count
     if i == N:
-        if L and curSum == S:
+        if curSum == S:
             count += 1
         return
 
-    solve(i+1, L + [nums[i]], curSum + nums[i])
-    solve(i+1, L, curSum)
+    solve(i+1, curSum + nums[i])
+    solve(i+1, curSum)
 
-solve(0, [], 0)
-print(count)
+solve(0, 0)
+print(count if S != 0 else count-1)
