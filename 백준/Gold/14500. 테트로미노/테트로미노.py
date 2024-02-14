0903119cf82f0f98e41443be2e42504d823d7e31
Minuring <144205824+Minuring@users.noreply.github.com>
2024-02-14 14:03:32 +0900

    [Gold IV] Title: 테트로미노, Time: 5728 ms, Memory: 38132 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+visited = [[False for _ in range(M)] for _ in range(N)]
+maxScore = -float('inf')
+def put(i, j, count, score,progress):
+    global maxScore
+
+    if count == 4:
+        # if score > maxScore:print(progress, score)
+        maxScore = max(maxScore, score)
+    else:
+        for dy, dx in zip([1,0,0], [0,-1,1]):
+            ny, nx = i+dy, j+dx
+            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+
+                visited[ny][nx] = True
+
+                if count == 2:
+                    put(i, j, 3, score+board[ny][nx], progress + [(ny, nx)])
+                put(ny, nx, count+1, score+board[ny][nx], progress + [(ny,nx)])
+
+                visited[ny][nx] = False
+
+
+for y in range(N):
+    for x in range(M):
+        visited[y][x] = True
+        put(y, x, 1, board[y][x], [(y,x)])
+        visited[y][x] = False
+
+print(maxScore)
