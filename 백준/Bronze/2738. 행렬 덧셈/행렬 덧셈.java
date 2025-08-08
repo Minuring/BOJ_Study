c5a71883fa77b40f1b65857e487ed06c289480bd
Minuring <144205824+Minuring@users.noreply.github.com>
2025-08-08 23:40:09 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 480 ms, Memory: 44244 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,22 +1,45 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    //단계별_2차원배열_행렬덧셈
-    public static void main(String[] args){
-        int N=0, M=0;
-        Scanner in = new Scanner(System.in);
-        N = in.nextInt(); M = in.nextInt();
-        int S[][] = new int[N][M];
-        for(int k=0; k<2; k++){
-            for(int i=0; i<N; i++){
-                for(int j=0; j<M; j++){
-                    S[i][j] += in.nextInt();
-                }
-            }
-        }
-        for(int[] line:S){
-            for(int e:line) System.out.print(e+" ");
-            System.out.println();
-        }
-    }
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		var line = br.readLine().split(" ");
+
+		var n = Integer.parseInt(line[0]);
+		var m = Integer.parseInt(line[1]);
+
+		var metrix1 = createMetrix(n, m);
+		var metrix2 = createMetrix(n, m);
+
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				var sum = metrix1[i][j] + metrix2[i][j];
+				sb.append(String.format("%d ", sum));
+			}
+
+			sb.append('\n');
+		}
+
+		System.out.print(sb);
+	}
+
+	static int[][] createMetrix(int n, int m) throws IOException {
+		int[][] metrix = new int[n][m];
+		
+		for(int i=0; i<n; i++) {
+			var elements = br.readLine().split(" ");
+			var row = new int[m];
+			for(int j=0; j<m; j++) {
+				row[j] = Integer.parseInt(elements[j]);
+			}
+
+			metrix[i] = row;
+		}
+
+		return metrix;
+	}
 }
