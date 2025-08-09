2cd968c928932bfe947a19580b53510bec5e30cf
Minuring <144205824+Minuring@users.noreply.github.com>
2025-08-09 11:26:57 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,28 +1,35 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
 
-        byte[][] map = new byte[100][100];
-        //Arrays.fill(map,0);
-        byte N = in.nextByte();
-        for(byte i=0; i<N; i++){
-            byte x = (byte) (in.nextByte()+10);
-            byte y = (byte) (in.nextByte()+10);
-            for(byte xpos = (byte) (x-10); xpos<x; xpos++){
-                for(byte ypos = (byte) (y-10); ypos<y; ypos++){
-                    map[xpos][ypos] = 1;
-                }
-            }
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+
+        var board = new int[100][100];
+
+        var count= Integer.parseInt(br.readLine());
+        for(int i=0; i<count; i++) {
+        	var split = br.readLine().split(" ");
+        	var col = Integer.parseInt(split[0]);
+        	var row = Integer.parseInt(split[1]);
+        	fill(board, col, row);
         }
-        int sum=0;
-        for(byte[] l : map){
-            for(byte b : l) if(b==1) sum++;
+
+        var sum = 0;
+        for(int i=0; i<100; i++) {
+        	for(int j=0; j<100; j++) {
+        		sum += board[i][j];
+        	}
         }
         System.out.print(sum);
+    }
 
+    static void fill(int[][] board, int r, int c) {
+    	for(int i=r; i<r+10; i++) {
+    		for(int j=c; j<c+10; j++) {
+    			board[i][j] = 1;
+    		}
+    	}
     }
 }
