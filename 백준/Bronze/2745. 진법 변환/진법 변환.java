6e229b5d6527abcbb452555a29ab45f3833695d0
Minuring <144205824+Minuring@users.noreply.github.com>
2025-08-12 00:00:20 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,26 +1,25 @@
 import java.io.*;
-import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String s[] = br.readLine().split(" ");
+        String[] split = br.readLine().split(" ");
+        char[] chars = split[0].toCharArray();
 
-        byte B=Byte.parseByte(s[1]); //입력 수의 진법
-        String number=s[0];
-
-        int res = 0;
-        int mul=1;
-        for(int i=number.length()-1; i>=0; i--, mul*=B){
-            int tmp=0;
-            if (Character.isUpperCase(number.charAt(i))) {
-                tmp = number.charAt(i)-55;
-            }else{
-                tmp = number.charAt(i)-48;
+        int base = Integer.parseInt(split[1]);
+        
+        long sum = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c >= 'A' && c <= 'Z') {
+                int number = (c - 'A') + 10;
+                sum += (long) Math.pow(base, chars.length -i -1) * number;
+            } else {
+                sum += (long) Math.pow(base, chars.length -i -1) * (c - '0');
             }
-            res += tmp*mul;
         }
-        bw.write(Integer.toString(res));
-        bw.close();
+
+        System.out.println(sum);
     }
 }
