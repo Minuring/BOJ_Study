67edc01e2cd379b60f2a66261786dc0a41b3cb6a
Minuring <144205824+Minuring@users.noreply.github.com>
2025-09-01 20:48:03 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 172 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = new Scanner(System.in).nextInt();
+        System.out.printf("%d\n2", (long) number * number);
+    }
+}
