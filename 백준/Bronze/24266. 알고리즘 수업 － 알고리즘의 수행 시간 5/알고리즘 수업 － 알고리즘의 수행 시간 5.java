2cfc4a259575e38e8a5bda9a5192ccec60395f65
Minuring <144205824+Minuring@users.noreply.github.com>
2025-09-01 21:04:00 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.printf("%d\n3", (long)n*n*n);
+    }
+}
